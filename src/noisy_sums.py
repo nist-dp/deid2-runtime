8acd8e3b1d6d5a4d15091c5bafe86e8196d71df9
terranceliu <terranceliu.tl64@gmail.com>
2020-10-22 03:20:53 -0400

    fixed noisy sums

--- src/noisy_sums.py
+++ src/noisy_sums.py
@@ -22,27 +22,24 @@ MAX_EPSILON = 10
 STEP = 0.05
 df = []
 for epsilon in np.arange(STEP, MAX_EPSILON+STEP, STEP):
-    df_private = naively_add_laplace_noise(df_ground_truth, scale=SENSITIVTY / epsilon)
+    df_private = df_ground_truth.sum(axis=1).to_frame()
+    df_private.columns = ['count']
+    df_private = naively_add_laplace_noise(df_private, scale=SENSITIVTY / epsilon)
     df_private['epsilon'] = round(epsilon, 2)
     df.append(df_private.reset_index())
 df = pd.concat(df)
 cols = list(df.columns[-1:]) + list(df.columns[:-1])
 df = df[cols]
 df.sort_values(INDEX_COLS, inplace=True)
-
-# just to check this matches the benchmark score
-for epsilon in [1.0, 2.0, 10.0]:
-    mask = df['epsilon'] == epsilon
-    outputs = df[mask]
-    outputs = outputs.set_index(INDEX_COLS).values
-    scores, penalties = get_score(df_ground_truth.values, outputs)
-    print('{:.3f}'.format(scores.sum()))
-    print(penalties.mean(axis=0))
-
-df.set_index(INDEX_COLS, inplace=True)
-df = df.sum(axis=1).to_frame()
-df.columns = ['count']
 df.to_csv('./data/laplace_sums.csv')
 
+# # just to check this matches the benchmark score
+# for epsilon in [1.0, 2.0, 10.0]:
+#     mask = df['epsilon'] == epsilon
+#     outputs = df[mask]
+#     outputs = outputs.set_index(INDEX_COLS).values
+#     scores, penalties = get_score(df_ground_truth.values, outputs)
+#     print('{:.3f}'.format(scores.sum()))
+#     print(penalties.mean(axis=0))
 
 
